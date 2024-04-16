b06fb4439467816492a202b7b3c7acdca3225d58
idris <idris.gemici61@gmail.com>
2024-04-16 18:06:49 +0300

    Initial version

---
+++ src/test/java/com/myFirstSelenium/FirstSeleniumClass.java
@@ -0,0 +1,13 @@
+package com.myFirstSelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumClass {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com.tr");
+
+    }
+}
